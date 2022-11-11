14a538dd03f742eaf78d4a447ec51e9e7105ef44
Bayekula-hb <hobedbayekula@gmail.com>
2022-11-11 16:11:20 +0100

    feat: create the first design of my app music

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movies_app/music.class.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -13,110 +14,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePage extends State<MyHomePage> {
+  bool clickedButton = false;
+  List<Music> myListMusic = [
+    new Music("Mélodie venue du ciel", "Joel Kabwe Pastor",
+        "images/wepik-photo-mode-2022109-112914.png", "dddddddd"),
+    new Music("Fidèle", "Moise Mbiye Pastor",
+        "images/wepik-photo-mode-2022109-11237.png", "urlSong")
+  ];
+  late Music musicNow;
+
+  @override
+  void initState() {
+    super.initState();
+    musicNow = myListMusic[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     //throw UnimplementedError();
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Ciné Movies"),
+        title: new Text(
+          "Music Challenge",
+          textAlign: TextAlign.center,
+        ),
         foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        elevation: 8.0,
+        centerTitle: true,
       ),
       body: new Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         padding: EdgeInsets.all(05.0),
-        color: Colors.black,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Container(
-              width: MediaQuery.of(context).size.width,
-              alignment: Alignment.topLeft,
-              child: new Column(
-                children: <Widget>[
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      new Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(
-                            "Salut Bienvenue",
-                            textAlign: TextAlign.justify,
-                            style: new TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          new Text(
-                            "Suivez les détails de vos films ici",
-                            textAlign: TextAlign.start,
-                            style: new TextStyle(
-                              color: Colors.white54,
-                            ),
-                          ),
-                        ],
-                      ),
-                      new Column(
-                        children: <Widget>[
-                          new Icon(
-                            Icons.account_circle,
-                            color: Colors.white,
-                            size: 40.0,
-                          ),
-                        ],
-                      ),
-                    ],
+        color: Colors.grey[800],
+        child: new Center(
+          child: new Container(
+            //padding: EdgeInsets.all(20.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Image.asset("images/wepik-photo-mode-2022109-112914.png"),
+                new Text(
+                  "Melodie Venue Du ciel",
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
                   ),
-                ],
-              ),
-            ),
-            new Container(
-              width: MediaQuery.of(context).size.width / 2,
-              color: Colors.white54,
-              margin: EdgeInsets.all(10.0),
-              child: new Center(
-                child: new Column(
-                  children: <Widget>[
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        new Icon(
-                          Icons.search,
-                          color: Colors.white,
-                          size: 30.0,
-                        ),
-                        new Text(
-                          "Titre du film",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
-                    )
+                  textAlign: TextAlign.center,
+                ),
+                new Text(
+                  "Joel Kabwe Pastor",
+                  style: new TextStyle(
+                    color: Colors.white70,
+                    fontSize: 20.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                new Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    new IconButton(
+                      onPressed: PressButton,
+                      icon: Icon(Icons.skip_previous_outlined),
+                      color: Colors.white,
+                    ),
+                    new IconButton(
+                      onPressed: PressButton,
+                      icon: clickedButton
+                          ? Icon(Icons.play_arrow_outlined)
+                          : Icon(Icons.play_arrow),
+                      color: Colors.white,
+                    ),
+                    new IconButton(
+                      onPressed: PressButton,
+                      icon: Icon(Icons.skip_next_outlined),
+                      color: Colors.white,
+                    ),
                   ],
-                ),
-              ),
-            ),
-            new Row(
-              children: [
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[],
-                ),
+                )
               ],
             ),
-          ],
+          ),
         ),
-        //child: new Center(
-        /*child: new Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-         
-        ),*/
-        //),
       ),
     );
+  }
+
+  void PressButton() {
+    setState(() {
+      clickedButton = !clickedButton;
+    });
   }
 }
---
+++ lib/music.class.dart
@@ -0,0 +1,14 @@
+class Music {
+  late String titleSong;
+  late String authSong;
+  late String imgPath;
+  late String urlSong;
+
+  Music(String titleSong, String authSong, String imgPath, String urlSong) {
+    this.titleSong = titleSong;
+    this.authSong = authSong;
+    this.imgPath = imgPath;
+    this.urlSong = urlSong;
+  }
+
+}
